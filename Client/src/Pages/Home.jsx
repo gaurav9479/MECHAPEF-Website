f1db81bb301093215c9e5ab0d6214baa98adf1a1
gaurav <gauravshivmurat2@gmail.com>
2026-06-22 22:50:41 +0530

    Admin panel and UI enhancements - Part 26

--- Client/src/Pages/Home.jsx
+++ Client/src/Pages/Home.jsx
@@ -1,6 +1,7 @@
 import Navbar from "../components/Navbar/Navbar";
 import CinematicHero from "../components/CinematicHero/CinematicHero";
 import OurTeam from "../components/OurTeam/OurTeam";
+import NoticeBoard from "../components/NoticeBoard/NoticeBoard";
 import Stats from "../components/Stats/Stats";
 import Domains from "../components/Domains/Domains";
 import Join from "../components/Join/Join";
@@ -14,9 +15,10 @@ const Home = () => {
       <Navbar />
       <CinematicHero />
       <OurTeam />
-      <Stats />
-      <Domains />
-      <Join />
+      {/* <Stats /> */}
+      {/* <Domains /> */}
+      {/* <Join /> */}
+      <NoticeBoard />
       <Footer />
     </>
   );
