fc35b46ade5a0de77f9f87b0194cc2b1bd1d56b2
gaurav <gauravshivmurat2@gmail.com>
2026-07-06 22:41:25 +0530

    gear

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import BackgroundGears from "./components/BackgroundGears/BackgroundGears";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 if (import.meta.env.DEV) {
-  root.render(<App />);
+  root.render(
+    <>
+      <BackgroundGears />
+      <App />
+    </>
+  );
 } else {
   root.render(
     <React.StrictMode>
+      <BackgroundGears />
       <App />
     </React.StrictMode>
   );
